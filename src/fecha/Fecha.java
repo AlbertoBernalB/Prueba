2432ffbe5ffb6b4dba8db322e4ad5ac8730153f3
Admin <Admin@203p06-PC>
2017-02-10 09:43:23 +0100

    Primer punto

--- src/fecha/Fecha.java
+++ src/fecha/Fecha.java
@@ -6,6 +6,12 @@ public class Fecha {
 
    int dia;
    int mes;
-   int ano;
-    
-}
+   int ano;       
+
+    public Fecha(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+             
+}         
